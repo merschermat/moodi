8030cc4a939058fdd5cf7f66b8b2559041bb9fa4
Mateus Merscher <mateusmerscher@hotmail.com>
2020-05-24 20:20:08 -0300

    starting the front project

---
+++ components/header.jsx
@@ -0,0 +1,21 @@
+
+export default function Header() {
+    return (
+
+        <div id="header">
+
+            test
+            <style jsx>{`
+        #header {
+          padding: 0 0.5rem;
+          display: flex;
+          min-width: 100vw;
+          height: 70px;
+          flex-direction: column;
+          justify-content: center;
+          background-color:#cccccc;
+        }
+      `}</style>
+        </div >
+    )
+}
